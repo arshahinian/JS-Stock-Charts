0878d56ac8e6a19b61aaf767caf45339ca985864
Araz Shahinian <arshahinian@gmail.com>
2022-01-18 18:08:33 -0500

    what I have so far

--- public/index.js
+++ public/index.js
@@ -4,6 +4,53 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    let response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=30min&outputsize=12&apikey=3678b5957e6d4a4ebdab035926f5c366');
+
+    let result = await response.json();
+    console.log(result);
+
+    const { GME, MSFT, DIS, BNTX } = result;
+
+    const stocks = [GME, MSFT, DIS, BNTX];
+    var myChart =new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.map(value => value.datetime),
+            datasets: stocks.map( stock => ({
+                label: stock.meta.symbol,
+                data: stock.values.map(value => parseFloat(value.high)),
+                backgroundColor:  getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+            }))
+        }
+    });
+
+    // var myChart2 =new Chart(highestPriceChartCanvas.getContext('2d'), {
+    //     type: 'bar',
+    //     data: {
+    //         labels: stocks[0].map(value => this.name),
+    //         datasets: stocks.map( stock => ({
+    //             label: stock.meta.symbol,
+    //             data: stock.values.map(value => parseFloat(value.high)),
+    //             backgroundColor:  getColor(stock.meta.symbol),
+    //             borderColor: getColor(stock.meta.symbol),
+    //         }))
+    //     }
+    // });
+}
+function getColor(stock){
+    if(stock === "GME"){
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if(stock === "MSFT"){
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if(stock === "DIS"){
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
+    }
 }
 
 main()
